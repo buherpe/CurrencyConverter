0ff7dbbfd647ac65184c41adb754c6e0b30806a2
Anatoly Filyuk <buherpet.38@gmail.com>
2021-11-18 20:21:45 +0500

    вынесен в файл PrometheusService

--- CurrencyConverter.Service/Program.cs
+++ CurrencyConverter.Service/Program.cs
@@ -1,11 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Threading;
-using System.Threading.Tasks;
-using Helpers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Prometheus;
 using Serilog;
 
 namespace CurrencyConverter.Service
@@ -33,40 +29,5 @@ namespace CurrencyConverter.Service
                     services.AddSingleton<IExchangeRateApiClient, ExchangeRateApiComClient>();
                     services.AddSingleton<IExchangeRateApiWrapper, ExchangeRateApiComWrapper>();
                 });
-    }
-
-    public class PrometheusService : BackgroundService
-    {
-        private readonly ILogger _log = Log.ForContext<PrometheusService>();
-
-        private readonly IServiceScopeFactory _serviceScopeFactory;
-
-        private readonly IConfiguration _configuration;
-
-        private static readonly Counter TickTock = Metrics.CreateCounter("sampleapp_ticks_total", "Just keeps on ticking");
-
-        public PrometheusService(IServiceScopeFactory serviceScopeFactory, IConfiguration configuration)
-        {
-            _serviceScopeFactory = serviceScopeFactory;
-            _configuration = configuration;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
-        {
-            var hostname = _configuration.GetSection("Prometheus:Hostname").Value;
-            var port = int.Parse(_configuration.GetSection("Prometheus:Port").Value);
-
-            _log.Info($"Hostname: {hostname}, port: {port}");
-            
-            var server = new MetricServer(hostname, port);
-            server.Start();
-
-            while (!cancellationToken.IsCancellationRequested)
-            {
-                TickTock.Inc();
-
-                await Task.Delay(1000, cancellationToken);
-            }
-        }
     }
 }
