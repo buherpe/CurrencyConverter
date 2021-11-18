3cea0dc7d36496bc54c797575fa366177ae5f3f0
Anatoly Filyuk <buherpet.38@gmail.com>
2021-11-18 19:58:58 +0500

    добавлены миграции исключены логи

---
+++ CurrencyConverter.Service/Migrations/20211118145622_Migration1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace CurrencyConverter.Service.Migrations
+{
+    public partial class Migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
